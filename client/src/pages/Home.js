a244532a2bc2ba3974f0a12dbc18586586958d64
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-11-07 16:20:19 -0800

    add css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,20 +2,14 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-import Nav from "../components/Nav";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home = () => {
   return (
     <div className="container mt-5">
-      <img
-        id="mainPhoto"
-        src="./images/velvet.jpg"
-        alt="velvet"
-        className="img-fluid"
-      />
-      <Nav />
+      <img src="./images/velvet.jpg" alt="velvet" className="img-fluid" />
+
       <CategoryMenu />
 
       <ProductList />
