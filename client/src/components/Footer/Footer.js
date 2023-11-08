f7758b6eb0c3e56962b4594040877cb646c9391c
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-11-08 10:40:31 -0800

    add bg

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Footer(props) {
   return (
-    <footer className={`footer ${props.darkMode ? "dark" : ""}`}>
-      <small>© 2023 Velvet</small>
+    <footer>
+      <small>Kateryna Stetsenko</small>
+      <small>
+        <i class="bi bi-github"></i>
+        https://github.com/itcreativeusa
+      </small>
     </footer>
   );
 }
